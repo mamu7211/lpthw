e72099b94c31d8f88bfebb4f77ee993638f87ad6
martin <martin@seventy2eleven.de>
2014-01-28 09:15:54 +0100

    Exercises 7 and 8.

---
+++ ex7.py
@@ -0,0 +1,36 @@
+# prints a string
+print "Mary had a little lamb."
+# prints a format string with one variable
+print "Its fleece was white as %s." % 'snow'
+# prints a string again
+print "And everywhere that Mary went."
+# repeates a string ten times
+print "." * 10 # like that!
+
+# twelve declarations
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# prints concatenated word 'Cheese', preventing a newline
+print end1 + end2 + end3 + end4 + end5 + end6,
+# then prints concatenated word 'Burger', both are separated by space because of the comma...
+print end7 + end8 + end9 + end10 + end11 + end12
+# watch that comma at the end. try removing to see what happens...
+# ...essentially it prevents the linebreak and adds a space between the two concatenated strings
+
+# study drills
+# 1. commenting again
+# 2. will do that, done
+# 3. ok, instead of paper i'll use evernote, notebook 'dev-diary', note 'LPTHW Errors', tagged with dev, errors and lpthw
+# 4. ok, review that list before each new exercise
+# 5. ok
